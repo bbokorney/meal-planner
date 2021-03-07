41e44afbe2da65cc80a47921e98419603ea4f80b
Baker Bokorney <bbokorney@gmail.com>
2021-03-07 10:08:39 -0700

    Add snackbar for adding ingredients to shopping list

--- app/src/features/recipes/AddIngredientsToShoppingList.js
+++ app/src/features/recipes/AddIngredientsToShoppingList.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { selectRecipeById } from "./recipesSlice";
 import { addIngredientsToShoppingList } from "../shopping-list/shoppingListSlice";
 import LowPriorityIcon from "@material-ui/icons/LowPriority";
 import Button from "@material-ui/core/Button";
 
-export const AddIngredientsToShoppingList = ({ recipeId }) => {
+export const AddIngredientsToShoppingList = ({ recipeId, onClick }) => {
   const recipe = useSelector((state) => selectRecipeById(state, recipeId));
   const ingredients = recipe.ingredients;
 
   const dispatch = useDispatch();
-  const history = useHistory();
 
   const onAddIngredientsToShoppingListClicked = () => {
     const ingredientsText = ingredients.map((ingredient) => ingredient.text);
     dispatch(addIngredientsToShoppingList(ingredientsText));
-    history.push(`/shopping-list`);
+    onClick();
   };
 
   return (
-    <Button on>
+    <Button>
       <LowPriorityIcon onClick={onAddIngredientsToShoppingListClicked} />
     </Button>
   );
--- app/src/features/recipes/SingleRecipe.js
+++ app/src/features/recipes/SingleRecipe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectRecipeById } from "./recipesSlice";
@@ -6,10 +6,37 @@ import { AddIngredientsToShoppingList } from "./AddIngredientsToShoppingList";
 import { BackButtonHeader } from "./BackButtonHeader";
 import Button from "@material-ui/core/Button";
 import EditIcon from "@material-ui/icons/Edit";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+import { BOTTOM_BAR_HEIGHT } from "../../app/constants";
+
+const useStyles = makeStyles((theme) => ({
+  anchorOriginBottomLeft: {
+    bottom: theme.spacing(BOTTOM_BAR_HEIGHT),
+  },
+}));
 
 export const SingleRecipe = ({ match }) => {
   const { recipeId } = match.params;
   const recipe = useSelector((state) => selectRecipeById(state, recipeId));
+
+  const classes = useStyles();
+
+  const [snackBarOpen, setSnackBarOpen] = useState(false);
+
+  const handleAddIngredientsClick = () => {
+    setSnackBarOpen(true);
+  };
+
+  const handleSnackBarClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSnackBarOpen(false);
+  };
 
   const ingredients = recipe.ingredients.map((ingredient, index) => {
     return <li key={index}>{ingredient.text}</li>;
@@ -22,7 +49,10 @@ export const SingleRecipe = ({ match }) => {
   return (
     <article>
       <BackButtonHeader text={recipe.name} />
-      <AddIngredientsToShoppingList recipeId={recipe.id} />
+      <AddIngredientsToShoppingList
+        recipeId={recipe.id}
+        onClick={handleAddIngredientsClick}
+      />
       <Button component={Link} to={`/editRecipe/${recipe.id}`}>
         <EditIcon />
       </Button>
@@ -30,6 +60,29 @@ export const SingleRecipe = ({ match }) => {
       <ul>{ingredients}</ul>
       <h3>Steps</h3>
       <ol>{steps}</ol>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        classes={classes}
+        open={snackBarOpen}
+        autoHideDuration={4000}
+        onClose={handleSnackBarClose}
+        message="Ingredients added to shopping list"
+        action={
+          <React.Fragment>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleSnackBarClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
     </article>
   );
 };
